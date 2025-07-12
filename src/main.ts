2fc063c3ad12bf6fededbab929030f69c62506ba
Joseph Huamani | Nerk <josephhuamanilol@gmail.com>
2025-07-12 04:25:24 -0500

    fix

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   
   // Configuración de CORS
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:4200', 'http://localhost:5173'], // Puertos comunes de desarrollo frontend
+    origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:4200', 'http://localhost:5173', 'https://finnet.netlify.app'], // Puertos comunes de desarrollo frontend
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
